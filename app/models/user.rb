1b6ac4bedf7ed4823604324a008834983ab317f5
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-02-06 20:54:42 +0900

    会員新規登録・ログイン機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  validates :nickname, presence: true
+  validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :date_of_birth, presence: true
+
+end
