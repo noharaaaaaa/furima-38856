a9e084ff3aea56b68f9175c04e24577299dd9bc9
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-02-07 21:50:09 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,14 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
-  #has_one :order
+  #has_one :order 未実装のため
   
+  #空の投稿を保存できないようにする
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :days_id, numericality: { other_than: 1 }
+
 end
