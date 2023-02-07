d25ab8d7319e3ecca7e4b18e44155f9e5123994f
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-02-07 21:15:28 +0900

    ActiveHashと関連モデルの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :days
+  belongs_to :user
+
   has_one_attached :image
   belongs_to :user
   #has_one :order
