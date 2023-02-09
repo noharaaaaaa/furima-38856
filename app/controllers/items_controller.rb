13822a6238a8828ef6751b91c94f9cb7d4ed2f5c
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-02-09 21:03:43 +0900

    データの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :item_find, only: [:show, :edit]
 
   def new
     @item = Item.new
@@ -19,7 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   private
@@ -27,6 +33,10 @@ class ItemsController < ApplicationController
   def items_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :days_id, :price, :image).
     merge(user_id: current_user.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
   end
 
 end
