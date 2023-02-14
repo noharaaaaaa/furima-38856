f33ea5075252894b8eda83c67bc2f43e1dcb7124
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-02-14 18:09:15 +0900

    テストの追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -116,6 +116,12 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("User can't be blank")
       end
+
+      it 'itemが紐付いていないと購入できない' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
