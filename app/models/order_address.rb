670dd38c053cab7dc69c419295a2ac4098296057
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-02-14 16:01:05 +0900

    購入機能の実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,22 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :post_number, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id, :token
+
+  with_options presence: true do
+    validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank", allow_blank: true}
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, allow_blank: true }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(post_number: post_number, prefecture_id: prefecture_id, city: city, address: address, building: building,
+      phone_number: phone_number, order_id: order.id)
+  end
+
+end
