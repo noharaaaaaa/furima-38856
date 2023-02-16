ee55b473d6a91cbc3ad1bf17089aa2f44622200f
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-02-16 19:12:47 +0900

    deployファイルの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "~> 3.17.1"
 set :application, "furima-38856"
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "git@example.com:noharaaaaaa/furima-38856.git"
+set :repo_url, "git@github.com:noharaaaaaa/furima-38856.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
